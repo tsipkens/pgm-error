c51ddf5cdfcaaa55073259de4d009407b6b93287
tsipkens <tsipkens@gmail.com>
2021-05-28 00:11:17 -0400

    Minor changes to add_noise.m
    + Update header.
    + Update `out` output to include var

--- add_noise.m
+++ add_noise.m
@@ -1,20 +1,21 @@
 
-% ADD_NOISE  Simulates signal with Poisson, Gaussian, and shot-to-shot errors. 
+% ADD_NOISE  Simulates signal with Poisson, Gaussian, and multiplicative errors. 
 %  This considers multiple sources of noise: 
-% 
+%  
 %  INPUTS:
-%   s_bar - expected mean signal
-%   tau - shot-to-shot std. dev.
-%   theta - amplification / scaling factor
-%   gamma - Gaussian noise level
-%   n_shots - number of signals to generate
+%   s_bar     Expected mean signal
+%   tau       Multiplicative (shot-to-shot) variance
+%   theta     Amplification / scaling factor
+%   gamma     Gaussian noise level
+%   n_shots   Number of signals to generate
+%   seed      Seed for random number generator
 % 
 %  OUTPUTS:
-%   s - set of corrupted signals, with error added
-%   Ls - matrix square root of inverse covariance
-%   out.s_ave - average of observed signals at each time
-%       s_std - standard deviation of observed signals at each time
-%       s_tilda - set of single-shot signals (with shot-to-shot error)
+%   s         Set of corrupted signals, with error added
+%   Ls        Matrix square root of inverse covariance
+%   out.s_ave     Average of observed signals at each time
+%       s_std	  Standard deviation of observed signals at each time
+%       s_tilda	  Set of single-shot signals (with shot-to-shot error)
 % 
 %  ------------------------------------------------------------------------
 %  
@@ -23,7 +24,7 @@
 %   deviation of sqrt of the number of counts. If the data is unscaled
 %   the = 1. If the data is scaled, which generally improves
 %   inference, theta accounts for the normalization factor. As norm_fact
-%   increases, the noise level drops, a consequence of their being more
+%   increases, the noise level drops, a consequence of there being more
 %   counts in the data than the scaled b0 indicates.
 % 
 %  ADDITIVE GAUSSIAN NOISE: 
@@ -38,40 +39,49 @@
 %  AUTHOR: Timothy Sipkens
 
 function [s, Ls, out] = ...
-    add_noise(s_bar, tau, the, gam, N_shots)
+    add_noise(s_bar, tau, the, gam, N_shots, seed)
 
 if ~exist('N_shots', 'var'); N_shots = []; end
-if isempty(N_shots); N_shots = 1; end % by default only generate one signal
+if isempty(N_shots); N_shots = 1; end  % by default only generate one signal
+
+if ~exist('seed', 'var'); seed = []; end
+if isempty(seed); seed = 1; end
 
 
-N_s = length(s_bar); % length of each signal
+N_s = length(s_bar);  % length of each signal
 
 
 % Setup for random variables
-rng(1); % control randomness
-n = randn(1, N_shots); % standard normal random variable, realizes shot-to-shot error
-n_P = randn(N_s, N_shots); % standard normal random vector, realizes Poisson noise
-n_G = randn(N_s, N_shots); % standard normal random vector, realizes Gaussian noise
+rng(seed);  % control randomness
+n = randn(1, N_shots);  % standard normal random variable, realizes shot-to-shot error
+n_P = randn(N_s, N_shots);  % standard normal random vector, realizes Poisson noise
+n_G = randn(N_s, N_shots);  % standard normal random vector, realizes Gaussian noise
 
 
 % Generate observed signals by adding error terms
-s = s_bar * ones(1,N_shots) + ... % expected average signal (s_bar)
-    tau .* s_bar * n + ... % shot-to-shot error (delta)
-    sqrt(the) .* sqrt(s_bar*ones(1,N_shots) + tau.*s_bar*n) .* n_P + ... % Poisson noise (p)
-    gam .* n_G; % Gaussian noise (g)
+s = s_bar * ones(1,N_shots) + ...  % expected average signal (s_bar)
+    tau .* s_bar * n + ...  % shot-to-shot error (delta)
+    sqrt(the) .* sqrt(s_bar*ones(1,N_shots) + tau.*s_bar*n) .* n_P + ...  % Poisson noise (p)
+    gam .* n_G;  % Gaussian noise (g)
 
 
 % Calculate statistics of signals
 Gs = sparse( ...
-    tau^2 + (s_bar*s_bar') + ... % shot-to-shot contribution
-    the .* diag(s_bar)) + ... % Poisson contribution
+    tau^2 + (s_bar*s_bar') + ...  % shot-to-shot contribution
+    the .* diag(s_bar)) + ...  % Poisson contribution
     gam^2 .* speye(N_s);
-Ls = chol(inv(Gs)); % get matrix square root
+Ls = chol(inv(Gs));  % get matrix square root
 
-out.s_std = std(s,[],2); % standard deviation over the signals at each time
-out.s_ave = mean(s,2); % average of the signals at each time
+
+% Realized, standard deviation, average, and single shot.
+out.s_std = std(s,[],2);  % standard deviation over the signals at each time
+out.s_ave = mean(s,2);  % average of the signals at each time
 out.s_tilde = s_bar * ones(1, N_shots)+...
-    tau .* s_bar * n; % expected single-shot signal (s_tilda = s_bar + delta)
+    tau .* s_bar * n;  % expected single-shot signal (s_tilda = s_bar + delta)
+
+% Expected variance (not realized).
+out.s_var = tau ^ 2 .* s_bar .^ 2 + ...
+    the .* s_bar + gam ^ 2;
+
 
 end
-
